6bd7137cc854b336033af9f4d3f2997efe684c3c
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-04-17 16:14:06 -0300

    feat: add color in drawer

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -7,11 +7,24 @@ export default function Layout() {
       <Drawer.Screen
         name="index"
         options={{
+          drawerStyle: {
+            backgroundColor: '#black',
+            borderTopRightRadius: 10,
+            width: 250,
+            paddingTop: 20,
+          },
+          drawerActiveBackgroundColor: '#gray',
+          drawerLabelStyle: {
+            color: 'white',
+            fontSize: 16,
+            fontWeight: 'bold',
+          },
           drawerLabel: 'Início',
           title: 'Home',
           headerShown: false,
           headerLeft: true,
-          drawerIcon: ({ size, color }) => (
+          
+          drawerIcon: ({ size }) => (
             <Ionicons name="home" size={size} color="#24e35a" />
           ),
         }}
@@ -19,11 +32,24 @@ export default function Layout() {
       <Drawer.Screen
         name="calculadora"
         options={{
+          drawerStyle: {
+            backgroundColor: 'black',
+            borderTopRightRadius: 10,
+            width: 250,
+            paddingTop: 20,
+          },
+          
+          drawerActiveBackgroundColor: '#gray',
+          drawerLabelStyle: {
+            color: 'white',
+            fontSize: 16,
+            fontWeight: 'bold',
+          },
           drawerLabel: 'Calculadora',
           title: 'Calculadora',
           headerShown: false,
           headerLeft: true,
-          drawerIcon: ({ size, color }) => (
+          drawerIcon: ({ size }) => (
             <Ionicons name="calculator" size={size} color="#24e35a" />
           ),
         }}
@@ -31,11 +57,23 @@ export default function Layout() {
             <Drawer.Screen
         name="contador"
         options={{
+          drawerStyle: {
+            backgroundColor: '#black',
+            borderTopRightRadius: 10,
+            width: 250,
+            paddingTop: 20,
+          },
+          drawerActiveBackgroundColor: '#gray',
+          drawerLabelStyle: {
+            color: 'white',
+            fontSize: 16,
+            fontWeight: 'bold',
+          },
           drawerLabel: 'Contador',
           title: 'Contador',
           headerShown: false,
           headerLeft: true,
-          drawerIcon: ({ size, color }) => (
+          drawerIcon: ({ size }) => (
             <Ionicons name="person" size={size} color="#24e35a" />
           ),
         }}
@@ -43,6 +81,18 @@ export default function Layout() {
             <Drawer.Screen
         name="geradorSenha"
         options={{
+          drawerStyle: {
+            backgroundColor: '#black',
+            borderTopRightRadius: 10,
+            width: 250,
+            paddingTop: 20,
+          },
+          drawerActiveBackgroundColor: '#gray',
+          drawerLabelStyle: {
+            color: 'white',
+            fontSize: 16,
+            fontWeight: 'bold',
+          },
           drawerLabel: 'Gerador de Senha',
           title: 'Gerador de Senha',
           headerShown: false,
